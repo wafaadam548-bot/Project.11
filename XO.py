af48ca45571025e58cfdaa3d4251bf6d191468d7
Wafa Adam <wafaadam548@gmail.com>
2025-11-11 04:24:28 -0500

    printing the board for the game

--- XO.py
+++ XO.py
@@ -5,3 +5,8 @@ board=["-","-","-"
 cerrentplayer="x"
 winner="noun"        
 gamingRuning=True
+def printBoard(board):
+    print(board[0]+"|",board[1]+"|",board[2]+"|")
+    print(board[3]+"|",board[4]+"|",board[5]+"|")
+    print(board[6]+"|",board[7]+"|",board[8]+"|")
+printBoard(board)
